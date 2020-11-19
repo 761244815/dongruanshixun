4b239f5c0e976cd9accd2bcaf52ef7b3414a9fc4
761244815 <761244815@qq.com>
2020-11-19 14:57:24 +0800

    1119.5

--- shixunJava/src/com/neusoft/JDBCDemo6.java
+++ shixunJava/src/com/neusoft/JDBCDemo6.java
@@ -1,6 +1,7 @@
 package com.neusoft;
 
 import com.neusoft.domain.Emp;
+import com.neusoft.utils.JDBCUtils;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -43,5 +44,27 @@ public class JDBCDemo6 {
         stmt.close();
         conn.close();
         return  list;
+    }
+    // 调用JDBCUtils 工具类进行操作
+    public static List<Emp> findAll2() throws Exception {
+        List<Emp> list = new ArrayList<>();
+        Connection conn = JDBCUtils.getConnection();
+        String sql = "select * from emp";
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery(sql);
+        while ( rs.next()){
+            int empno = rs.getInt(1);
+            String eName = rs.getString(2);
+            String job = rs.getString(3);
+            int mgr = rs.getInt(4);
+            Date hiredata = rs.getDate(5);
+            int sal = rs.getInt(6);
+            int comm = rs.getInt(7);
+            int deptno = rs.getInt(8);
+            Emp emp = new Emp(empno, eName, job, mgr, hiredata, sal, comm, deptno);
+            list.add(emp);
+        }
+        JDBCUtils.close(rs, stmt, conn);
+        return list;
     }
 }
