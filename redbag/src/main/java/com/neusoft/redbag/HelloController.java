4901c6de67238d78f1592f11808e4a62af2d2d44
761244815 <761244815@qq.com>
2020-11-24 16:10:25 +0800

    1124.2

--- redbag/src/main/java/com/neusoft/redbag/HelloController.java
+++ redbag/src/main/java/com/neusoft/redbag/HelloController.java
@@ -4,12 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
 public class HelloController {
-
     @GetMapping("/")
     public String sayHello(){
-        return "Hello SpringBoot";
+        return "江科大 Java";
     }
-
 }
